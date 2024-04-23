a082e89328efe0462e14e95f27865d291b1a526c
unknown <shaik.rehamanrp@gmail.com>
2024-04-23 21:38:30 +0530

    added add_blog post and get urls and abiltiy to save blogs in the json

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+import fs from "fs";
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = 3000
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port,()=>{
     console.log(`Server Started on port ${port}`)
@@ -15,6 +18,28 @@ app.get("/", (req,res)=>{
 
 app.get("/blogs", (req,res)=>{
     res.send("<h1>Blogs</h1>")
+})
+
+app.get("/add_blog", (req,res)=>{
+    res.render("add_blog.ejs")
+})
+
+app.post("/blog_submit",(req,res)=>{
+    console.log(req.body)
+    //console.log(req.body["title"])
+    const data = [{
+        title : req.body["title"],
+        description : req.body["description"]
+    }]
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFile('blogs.json', jsonData, 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing to file:', err);
+        } else {
+            console.log('Data written to file successfully!');
+        }
+    });
+    res.redirect("/")
 })
 
 app.get("/about", (req,res)=>{
