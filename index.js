222f42365d6a72bea57788d7905316e69b7a7c1e
unknown <shaik.rehamanrp@gmail.com>
2024-04-23 22:07:17 +0530

    Added functionaly to add blogs and list blogs

--- index.js
+++ index.js
@@ -8,40 +8,72 @@ const port = 3000
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Started on port ${port}`)
 })
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
-app.get("/blogs", (req,res)=>{
-    res.send("<h1>Blogs</h1>")
-})
-
-app.get("/add_blog", (req,res)=>{
+app.get("/add_blog", (req, res) => {
     res.render("add_blog.ejs")
 })
 
-app.post("/blog_submit",(req,res)=>{
+app.post("/blog_submit", (req, res) => {
     console.log(req.body)
-    //console.log(req.body["title"])
-    const data = [{
-        title : req.body["title"],
-        description : req.body["description"]
-    }]
-    const jsonData = JSON.stringify(data, null, 2);
-    fs.writeFile('blogs.json', jsonData, 'utf8', (err) => {
+    fs.readFile('blogs.json', 'utf8', (err, data) => {
         if (err) {
-            console.error('Error writing to file:', err);
-        } else {
-            console.log('Data written to file successfully!');
+            console.error('Error reading the file:', err);
+            return;
+        }
+        try {
+            // Parse JSON data
+            const dataList = JSON.parse(data);
+
+            const new_data = {
+                title: req.body["title"],
+                description: req.body["description"]
+            }
+            dataList.push(new_data)
+            const jsonData = JSON.stringify(dataList, null, 2);
+            fs.writeFile('blogs.json', jsonData, 'utf8', (err) => {
+                if (err) {
+                    console.error('Error writing to file:', err);
+                } else {
+                    console.log('Data written to file successfully!');
+                }
+            });
+            res.redirect("/")
+
+        } catch (err) {
+            console.error('Error parsing JSON:', err);
         }
     });
-    res.redirect("/")
 })
 
-app.get("/about", (req,res)=>{
+app.get("/blogs", (req, res) => {
+    fs.readFile('blogs.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            return;
+        }
+        try {
+            // Parse JSON data
+            const dataList = JSON.parse(data);
+
+            // Log the list of dictionaries
+            console.log(dataList);
+            res.render("blogs.ejs", {
+                list: dataList
+            })
+        } catch (err) {
+            console.error('Error parsing JSON:', err);
+            res.render("blogs.ejs")
+        }
+    });
+})
+
+app.get("/about", (req, res) => {
     res.send("<h1>About</h1>")
 })
